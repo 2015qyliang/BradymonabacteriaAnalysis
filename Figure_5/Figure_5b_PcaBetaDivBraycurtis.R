6f0d4cfa56d4326dde69e29b57a78970b2cd2dc7
qyliang <qfsfdxlqy@163.com>
2020-07-02 22:55:14 +0800

    Add files via upload

--- Figure_5/Figure_5b_PcaBetaDivBraycurtis.R
+++ Figure_5/Figure_5b_PcaBetaDivBraycurtis.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(ggfortify)
 library(vegan) # 2.5-6
 
-file.df <- read.table('Figure_5b_BraycrutisMetricEnvirType.txt',
+file.df <- read.table(gzfile('Figure_5b_BraycrutisMetricEnvirType.txt'),
                       header = F,sep = '\t',row.names = 1)
 colnames(file.df) <- c( rownames(file.df),"EnvirTypes", "salineType" )
 df <- file.df[,c(1:(length(rownames(file.df))))]
@@ -24,7 +24,7 @@ p <- autoplot(pca, data = file.df,
   theme( panel.background = element_rect(fill="white",color="black"),
          panel.grid = element_blank(), legend.position = "right",
          legend.key = element_blank())
-
+p
 # Export Set:
 # 4*6
 
@@ -32,3 +32,5 @@ p <- autoplot(pca, data = file.df,
 #        units = 'in', width = 6, height = 4)
 
 # following modified by PhantomPDF (Foxit) software
+
+
